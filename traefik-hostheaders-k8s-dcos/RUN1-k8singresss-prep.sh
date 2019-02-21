440fc058ee98a5d24ff78e9096c80df867817615
Eric Bannon <34694236+ericbannon@users.noreply.github.com>
2019-02-21 12:27:48 -0800

    Update and rename RUN1-dcos-k8sprep-install.sh to RUN1-k8singresss-prep.sh

---
+++ traefik-hostheaders-k8s-dcos/RUN1-k8singresss-prep.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get DC/OS Master Node URL
+
+echo
+echo " #########################################################"
+echo " ### Kubernetes API-Server Configuration Install  ###"
+echo " #########################################################"
+echo
+
+# Configure api server and deploy Kubernetes Dashboard over Local Host using Kube proxy
+
+        read -p 'Enter Public IP of the Kubelet manually (COMPLETED): ' PUBLICNODEIP
+        PUBLICNODEIP=$PUBLICNODEIP
+
+echo "Configuring Kubeconfig to use the DCOS-Kubernetes API Server URL for Frontend Traffic and Kubernetes Dashboard"
+sleep 10
+echo
+
+kubcetl config set --context-name=default --apiserver-url https://$PUBLICNODEIP:6443 --insecure-skip-tls-verify --name="kubernetes"
+
+echo
+echo
+echo
+echo .
+
+echo "Finished! You can now execute the traefik.sh script to deploy the example web app with ingress and hostname headers"
+echo
+echo .
+
+echo "Starting Kubctl Dashboard over localhost at http://127.0.0.1:8001/api/v1/namespaces/kube-system/services/http:kubernetes-dashboard:/proxy/
+
+NOTE: Please open a new terminal to keep kubectl proxy running on your local host!
+You can now proceed to RUN2 in order to install the Traefik Controller and example web services for ingress"
+
+kubectl proxy
